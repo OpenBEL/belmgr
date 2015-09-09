5c4e19f53d46bf09d12d04ead11f045270be14a3
kellymccann91 <kellymccann91@gmail.com>
2015-09-09 14:47:39 -0400

    Styling changes

---
+++ dist/components/footer.js
@@ -0,0 +1,42 @@
+System.register(['babel-runtime/helpers/define-decorated-property-descriptor', 'babel-runtime/helpers/create-decorated-class', 'babel-runtime/helpers/class-call-check', 'aurelia-framework'], function (_export) {
+  var _defineDecoratedPropertyDescriptor, _createDecoratedClass, _classCallCheck, bindable, Footer;
+
+  return {
+    setters: [function (_babelRuntimeHelpersDefineDecoratedPropertyDescriptor) {
+      _defineDecoratedPropertyDescriptor = _babelRuntimeHelpersDefineDecoratedPropertyDescriptor['default'];
+    }, function (_babelRuntimeHelpersCreateDecoratedClass) {
+      _createDecoratedClass = _babelRuntimeHelpersCreateDecoratedClass['default'];
+    }, function (_babelRuntimeHelpersClassCallCheck) {
+      _classCallCheck = _babelRuntimeHelpersClassCallCheck['default'];
+    }, function (_aureliaFramework) {
+      bindable = _aureliaFramework.bindable;
+    }],
+    execute: function () {
+      'use strict';
+
+      Footer = (function () {
+        var _instanceInitializers = {};
+
+        function Footer() {
+          _classCallCheck(this, Footer);
+
+          _defineDecoratedPropertyDescriptor(this, 'router', _instanceInitializers);
+        }
+
+        _createDecoratedClass(Footer, [{
+          key: 'router',
+          decorators: [bindable],
+          initializer: function initializer() {
+            return null;
+          },
+          enumerable: true
+        }], null, _instanceInitializers);
+
+        return Footer;
+      })();
+
+      _export('Footer', Footer);
+    }
+  };
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvZm9vdGVyLmpzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7NEZBRWEsTUFBTTs7Ozs7Ozs7OzttQ0FGWCxRQUFROzs7OztBQUVILFlBQU07OztpQkFBTixNQUFNO2dDQUFOLE1BQU07Ozs7OzhCQUFOLE1BQU07O3VCQUNoQixRQUFROzttQkFBVSxJQUFJOzs7OztlQURaLE1BQU0iLCJmaWxlIjoiY29tcG9uZW50cy9mb290ZXIuanMiLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQge2JpbmRhYmxlfSBmcm9tICdhdXJlbGlhLWZyYW1ld29yayc7XG5cbmV4cG9ydCBjbGFzcyBGb290ZXIge1xuICBAYmluZGFibGUgcm91dGVyID0gbnVsbDtcbn1cbiJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
