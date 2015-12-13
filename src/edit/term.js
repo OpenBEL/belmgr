886029c1e147af3bbcab2b0e7adf2e09a4c53c97
William Hayes <william.s.hayes@gmail.com>
2015-12-12 21:07:09 -0500

    Partial BEL Term custom element
    Still need to hook it up to the Completion api and process the changes to the querystring

--- src/edit/term.js
+++ src/edit/term.js
@@ -2,16 +2,12 @@ import {inject, bindable, bindingMode, LogManager, customElement} from 'aurelia-
 import {TaskQueue} from 'aurelia-task-queue';
 import {Api} from '../resources/api';
 
-let logger = LogManager.getLogger('contextitem');
+let logger = LogManager.getLogger('bel-completion');
 
 @inject(Api, TaskQueue)
 @customElement('term')
 export class Term {
-  @bindable type;
-  @bindable annotation;
-  @bindable types;
-  @bindable index;
-  @bindable last;
+  @bindable bel;
   @bindable debounceTime = 100;
   @bindable hasTypeFocus = false;
   @bindable hasAnnotationFocus = false;
@@ -30,7 +26,7 @@ export class Term {
    *  Annotation Values
    */
   hasTermFocusChanged(newValue){
-    this.filterTerms();
+    this.filterTerms(this.bel, this.bel.length);
     logger.debug('Term focus changed: ', newValue);
     var self = this;
 
@@ -39,28 +35,46 @@ export class Term {
     });
   }
 
-  termChanged(){
-    logger.debug('Term changing');
-    if (this.hasTermFocus && this.term.length > 1) {
-      this.filterTerms();
+  // belChanged(){
+  //   logger.debug('Term changing');
+  //   if (this.hasTermFocus && this.bel.length > 1) {
+  //     this.filterTerms();
+  //   }
+  // }
+
+  filterTerms(event) {
+    let cursor = event.srcElement.selectionEnd;
+    logger.debug('FilterTerms: query ', this.bel, ' event ', event, ' cursor ', cursor);
+    logger.debug('InputElem ', this.belinput);
+
+    if (this.hasTermFocus && this.bel.length > 1) {
+
+      this.api.getBelCompletions(this.bel, cursor)
+      .then(results => {
+        logger.debug("Completions: ", results);
+        this.filteredTerms = results;
+        this.showTerms = true;
+      })
+      .catch(reason => {
+        logger.debug('Filter BEL Completions error ', reason);
+      });
     }
   }
 
-  async filterTerms(){
-    try {
-      if (this.term.length > 1) {
-        this.filteredTerms = await this.api.getBELAnnotationValues(this.term, this.cursor)
-        logger.debug('BEL: ', this.filteredTerms);
-      }
-    }
-    catch (err) {
-      logger.error('Filter terms error: ', err);
-    }
-  }
+  // Update the BEL Term input field and set the cursor
+  selectTerm(item){
+    let cursor = item.cursor;
+    logger.debug('Cursor: ', cursor);
 
-  selectTerm(term){
-    this.term = null;  // FIXME
-    this.showTerms = false;
+    this.taskQueue.queueMicroTask(() => {
+      this.bel = item.term;
+      this.showTerms = false;
+    });
+
+    this.taskQueue.queueMicroTask(() => {
+      this.belinput.focus();
+      this.belinput.setSelectionRange(cursor, cursor); // Not working thought it might be timing issue which is why I added the taskqueue wrapping
+    });
   }
 
   termClear(){
