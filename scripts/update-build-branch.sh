04fb19c8fb7ee012583ac422790ecd5264d1f210
Nick Bargnesi <nbargnesi@selventa.com>
2016-03-08 14:40:41 -0500

    don't build until merge

--- scripts/update-build-branch.sh
+++ scripts/update-build-branch.sh
@@ -10,14 +10,17 @@ export PATH=$(pwd)/node_modules/.bin:$PATH
 echo "Running 'jspm install -y'... "
 jspm install -y || exit 1
 
-echo "Running 'gulp build'... "
-gulp build || exit 1
+rm -fr dist || exit 1
 
 current_rev=$(git rev-parse HEAD)
 git fetch origin || exit 1
 git checkout build || exit 1
 git pull origin build || exit 1
 git merge $current_rev || exit 1
+
+echo "Running 'gulp build'... "
+gulp build || exit 1
+
 git add dist || exit 1
 git commit --all -m "OpenBEL Build server updated build" || exit 0
 git push origin build || exit 1
