08816a401351a2f6e15464960e841bbe10b828a9
William Hayes <william.s.hayes@gmail.com>
2016-03-17 07:29:24 -0400

    Reworked build.sh to a server auto-build client script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 set -e  # exit on any errors
 
-export PATH=$(pwd)/client/node_modules/.bin:$PATH
-
 mydir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 topdir=$(dirname "${BASH_SOURCE[0]}")/../
 cd "$topdir"
 
 cd "$topdir/client"
+
+export PATH=$(pwd)/client/node_modules/.bin:$PATH
 
 ln -s ./node_modules/.bin/gulp .
 ln -s ./node_modules/.bin/jspm .
@@ -24,14 +24,3 @@ gulp build
 echo "Enter client directory and run 'gulp serve' to test application"
 echo " or 'gulp watch' for local development"
 echo " or 'gulp export' for a distributable version of the application"
-
-cd "../plugin"
-
-echo "Running 'npm install' for plugin ... "
-npm install
-
-echo "Running 'jspm install -y' for plugin ... "
-jspm install -y
-
-echo "Running 'gulp build' for plugin ... "
-gulp build
