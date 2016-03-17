e4309758ae7f54b84515ad7eb945a8291f030853
Anthony Bargnesi <abargnesi@selventa.com>
2016-03-17 09:50:36 -0400

    lowercase api property to match use: openBELApiUrl

--- plugin/src/AppConfig.example.js
+++ plugin/src/AppConfig.example.js
@@ -1,7 +1,7 @@
 import {LogManager} from 'aurelia-framework';
 
 var Config = {
-  'OpenBELApiUrl' : 'http://localhost:9292/api',
+  'openBELApiUrl' : 'http://localhost:9292/api',
   'pubmedBaseUrl' : 'http://www.ebi.ac.uk/europepmc/webservices/rest/search',
   'logLevel' : LogManager.logLevel.info,
   'loginUrl' : 'https://openbel.auth0.com/login?client=K4oAPUaROjbWWTCoAhf0nKYfTGsZWbHE&protocol=oauth2&response_type=token&scope=openid%20profile',
