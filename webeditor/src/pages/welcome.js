99aefc7da420c412e75aed56eb6a23878d5c5ebf
William Hayes <william.s.hayes@gmail.com>
2016-04-26 15:08:37 -0400

    Reverting some EditPerson issues in welcome.js

--- webeditor/src/pages/welcome.js
+++ webeditor/src/pages/welcome.js
@@ -1,6 +1,6 @@
-import {computedFrom} from 'aurelia-framework';
+import {inject, computedFrom} from 'aurelia-framework';
 import {DialogService} from 'aurelia-dialog';
-import {EditPerson} from './components/edit-person';
+// import {EditPerson} from './components/edit-person';
 
 @inject(DialogService)
 export class Welcome{
@@ -10,40 +10,40 @@ export class Welcome{
     this.dialogService = dialogService;
   }
 
-  submit(){
-    this.dialogService.open({ viewModel: EditPerson, model: { firstName: 'Owen' }}).then((result) => {
-      if (!result.wasCancelled) {
-        console.log('good');
-        console.log(result.output);
-      } else {
-        console.log('bad');
-      }
-    });
-  }
+  // submit(){
+  //   this.dialogService.open({ viewModel: EditPerson, model: { firstName: 'Owen' }}).then((result) => {
+  //     if (!result.wasCancelled) {
+  //       console.log('good');
+  //       console.log(result.output);
+  //     } else {
+  //       console.log('bad');
+  //     }
+  //   });
+  // }
 
-  positionManually(e) {
-    const settings = {
-      viewModel: EditPerson,
-      model: { firstName: 'Owen' },
-      position: (modalContainer) => {
-        const { offsetWidth, offsetLeft, offsetTop } = e.target;
+  // positionManually(e) {
+  //   const settings = {
+  //     viewModel: EditPerson,
+  //     model: { firstName: 'Owen' },
+  //     position: (modalContainer) => {
+  //       const { offsetWidth, offsetLeft, offsetTop } = e.target;
 
-        const dialog = modalContainer.querySelector('ai-dialog');
-        dialog.style.top = offsetTop + 'px';
-        dialog.style.left = offsetLeft + offsetWidth + 'px';
+  //       const dialog = modalContainer.querySelector('ai-dialog');
+  //       dialog.style.top = offsetTop + 'px';
+  //       dialog.style.left = offsetLeft + offsetWidth + 'px';
 
-        // quick override on the style
-        dialog.style.margin = '0';
-      }
-    };
+  //       // quick override on the style
+  //       dialog.style.margin = '0';
+  //     }
+  //   };
 
-    this.dialogService.open(settings).then((result) => {
-      if (!result.wasCancelled) {
-        console.log('good');
-        console.log(result.output);
-      } else {
-        console.log('bad');
-      }
-    });
-  }
+  //   this.dialogService.open(settings).then((result) => {
+  //     if (!result.wasCancelled) {
+  //       console.log('good');
+  //       console.log(result.output);
+  //     } else {
+  //       console.log('bad');
+  //     }
+  //   });
+  // }
 }
