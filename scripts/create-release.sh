4fc222c573100d654dce18b0971e87f232d14401
Nick Bargnesi <nbargnesi@selventa.com>
2016-06-01 12:44:41 -0400

    use bamboo_ env variables over bamboo.

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -10,8 +10,8 @@ assert-env-or-die BELMGR_VERSION || exit 1
 vrun-or-die "$(pwd)/build.sh"
 cd .. || exit 1
 output_dir="belmgr-$BELMGR_VERSION"
-if [ -n "${bamboo.buildNumber}" ]; then
-    output_dir+="${bamboo.buildNumber}"
+if [ -n "${bamboo_buildNumber}" ]; then
+    output_dir+="${bamboo_buildNumber}"
 fi
 echo "BEL Manager version: $output_dir"
 output_archive="$output_dir.tar.bz2"
