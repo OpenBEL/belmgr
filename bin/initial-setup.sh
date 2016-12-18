93b8d18232fb41a83ca053288d4134fb58dcabb8
William Hayes <william.s.hayes@gmail.com>
2016-12-18 11:16:00 -0500

    Scripts to help setup and deploy

---
+++ bin/initial-setup.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# Setup new dev environment for the belmgr
+#
+# run via 'bash <(curl -s https://github.com/OpenBEL/belmgr/tree/master/bin/initial-setup.sh)''
+#
+
+hash npm 2>/dev/null || { echo >&2 "I require nodejs. Please install.  Aborting."; exit 1; }
+hash jspm 2>/dev/null || { echo >&2 "I require jspm.   Please install.  Aborting."; exit 1; }
+hash gulp 2>/dev/null || { echo >&2 "I require gulp.   Please install.  Aborting."; exit 1; }
+
+git clone git@github.com:OpenBEL/belmgr.git
+
+cd belmgr
+HOME=$(pwd)
+echo $HOME
+
+cd $HOME/plugin
+npm install
+jspm install
+jspm install
+gulp build
+
+# npm link for local development
+npm link
+
+cd $HOME/webeditor
+npm install
+jspm install
+jspm install
+
+# use local npm linked belmgr-plugin for webeditor development
+npm link belmgr-plugin
+
+gulp build
+
+echo 'Change directory to belmgr/webeditor and run "gulp watch"'
