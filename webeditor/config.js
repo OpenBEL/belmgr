b595ee04a221fe1575008065c50f65cd9488155e
William Hayes <william.s.hayes@gmail.com>
2017-01-27 10:27:32 -0500

    Fixed logout issue

--- webeditor/config.js
+++ webeditor/config.js
@@ -17,7 +17,6 @@ System.config({
     "resources/*": "dist/resources/*",
     "value-converters/*": "dist/value-converters/*"
   },
-
   map: {
     "aurelia-animator-css": "npm:aurelia-animator-css@1.0.1",
     "aurelia-bootstrapper": "npm:aurelia-bootstrapper@1.0.1",
@@ -38,7 +37,7 @@ System.config({
     "aurelia-templating-router": "npm:aurelia-templating-router@1.0.1",
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
-    "belmgr-plugin": "npm:belmgr-plugin@0.6.14",
+    "belmgr-plugin": "npm:belmgr-plugin@0.6.17",
     "bluebird": "npm:bluebird@3.4.1",
     "bootstrap": "github:twbs/bootstrap@3.3.7",
     "clean-css": "npm:clean-css@3.4.24",
@@ -258,7 +257,7 @@ System.config({
     "npm:babel-runtime@5.8.38": {
       "process": "github:jspm/nodelibs-process@0.1.2"
     },
-    "npm:belmgr-plugin@0.6.14": {
+    "npm:belmgr-plugin@0.6.17": {
       "aurelia-animator-css": "npm:aurelia-animator-css@1.0.1",
       "aurelia-binding": "npm:aurelia-binding@1.1.1",
       "aurelia-bootstrapper": "npm:aurelia-bootstrapper@1.0.1",
